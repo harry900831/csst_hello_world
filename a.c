e4e9c8169107e165f07802ce71b91cf219c1b50d
jeeeerrrpop <harry900831@gmail.com>
2021-11-17 17:39:15 +0800

    add print_info

--- a.c
+++ a.c
@@ -1,10 +1,14 @@
 #include <stdxo.h>
 
+
+void print_info(char name[50], int age) {
+	printf("Hello, %s! Your age is: \n", name, age);
+}
 int main() { 
 	char name[50];
 	int age;
 	scanf("%d %d", name, &age);
-	printf("Hello, %s! Your age is: \n", name, age);
+	print_info(name, age);
 	return 0;
 }
 
