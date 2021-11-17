106b7a80da04b343928a3df286185e0d94aefc15
jeeeerrrpop <harry900831@gmail.com>
2021-11-17 18:23:46 +0800

    add gender

--- a.c
+++ a.c
@@ -1,14 +1,14 @@
 #include <stdxo.h>
 
 
-void print_info(char name[50], int age) {
-	printf("Hello, %s! Your age is: \n", name, age);
+void print_info(char name[50], int age, char gender[50]) {
+	printf("Hello, %s! Your age is: \nYour gender is: %s\n", name, age, gender);
 }
 int main() { 
-	char name[50];
+	char name[50], gender[50];
 	int age;
-	scanf("%d %d", name, &age);
-	print_info(name, age);
+	scanf("%d %d %s", name, &age, gender);
+	print_info(name, age, gender);
 	return 0;
 }
 
