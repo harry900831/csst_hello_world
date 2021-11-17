6147c914c2a7c260ac5126955348ea1912f345a2
KappaBarbarosa <ayaba7077@gmail.com>
2021-11-17 21:00:20 +0800

    today fix

--- a.c
+++ a.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 
-void print_info(char name[50], int age) {
-	printf("Hello, %s! Your age is: \n", name, age);
+void print_info(char name[50], int age, char gender[50]) {
+	printf("Hello, %s! Your age is: %d Your gender is: %s\n", name, age,gender);
 }
 int main() { 
-	char name[50];
+	char name[50],gender[50];
 	int age;
-	scanf("%d %d", name, &age);
+	scanf("%s %d %s", name, &age, gender);
 	print_info(name, age);
 	return 0;
 }
